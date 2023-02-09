27d97d530098fcd2a6f735feffec28c3c871a530
Ali Yasin Eser <aliyasineser@gmail.com>
2023-02-09 02:37:34 +0100

    Meteomatics code in the project

--- Weather/ContentView.swift
+++ Weather/ContentView.swift
@@ -6,16 +6,36 @@
 //
 
 import SwiftUI
+import CoreLocation
 
 struct ContentView: View {
+    var meteo: Meteomatics
+
+    var date = Date()
+//    Full list of available parameters at https://www.meteomatics.com/en/api/available-parameters/alphabetic-list/
+    var parameters = ["t_2m:C", "wind_speed_850hPa:ms"]
+    var latitude = 52.520551
+    var longitude = 13.461804
+    var user = ""
+    var password = ""
+
+    init() {
+        meteo = Meteomatics(forDate: date, parameters: self.parameters, latitude: self.latitude, longitude: self.longitude, user: self.user, password: self.password)
+    }
+
+    @State var weather: MeteomaticsData = MeteomaticsData()
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text("\(weather.data[0].coordinates[0].dates[0].value)")
+            .task {
+                do {
+                    weather = try await meteo.fetchWeather()
+                } catch {
+                    print(error)
+                }
+            }
+
+            .padding()
+
     }
 }
 
